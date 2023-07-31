28e4e8139b31e004f97a95ad9e04da89d8dbf95e
tecfu <help@tecfu.com>
2023-07-30 22:20:20 -0700

    feat: Add convert-json-to-sqs-event script

---
+++ .scripts/convert-json-to-sqs-event.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#################################
+#
+# Usage
+#
+# $cat json-array.json | ./convert-json-to-sqs-event.sh
+#################################
+
+input=$(cat)
+epoch=$(date +%s)
+header=$(echo '{"Records": ')
+# from: https://stackoverflow.com/a/41433037/3751385
+body=$(echo $input | jq --arg START $epoch '($START | tonumber) as $s
+  | to_entries
+  | map({messageId: ($s + .key), "body": (.value | tojson | sub("\""; "\"")) })')
+footer=$(echo '}')
+echo $header$body$footer | jq .
