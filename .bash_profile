cec95499b08d686e850aef3e422954b2d0a077f5
tecfu <help@tecfu.com>
2025-02-19 20:56:05 -0700

    chore: moved custom envs to profile-env-private

--- .bash_profile
+++ .bash_profile
@@ -9,5 +9,3 @@ fi
 if [[ -f ~/.bashrc ]]; then
   source ~/.bashrc
 fi
-export VOLTA_HOME="$HOME/.volta"
-export PATH="$VOLTA_HOME/bin:$PATH"
--- profile-env-public.sh
+++ profile-env-public.sh
@@ -10,16 +10,3 @@ export GIT_PAGER=""
 # Ignore commands leading with a space in terminal history
 # https://stackoverflow.com/a/29188490/3751385
 export HISTCONTROL=ignoreboth
-
-# For jupyter-lab. See: https://stackoverflow.com/a/67130797/3751385
-export PATH=$PATH:$HOME/.local/bin
-
-# For Android studio
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-#
-export NVIM_CONFIG=cmp
