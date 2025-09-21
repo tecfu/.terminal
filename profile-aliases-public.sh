466420782c0e5777118dafbf872ecf8de01ec4c3
tecfu <help@tecfu.com>
2025-09-21 15:19:24 -0700

    refactor: Filter out private aliases

--- profile-aliases-public.sh
+++ profile-aliases-public.sh
@@ -3,24 +3,16 @@
 # Aliases
 #
 #################################
-# aider
-function aiderpop() {
-  alacritty --class Aider,Aider -e aider "$@"
-}
-
-# bookmark
-alias vm='bm'
-
 # fzgrep
-alias fzgrep='~/.terminal/scripts/fzgrep.sh'
+alias alias-fzgrep='~/.terminal/scripts/fzgrep.sh'
 
 # IP lookup
-alias ipv4lookup='dig +short myip.opendns.com @resolver1.opendns.com'
-alias ipv6lookup='ip -6 addr'
+alias alias-ipv4lookup='dig +short myip.opendns.com @resolver1.opendns.com'
+alias alias-ipv6lookup='ip -6 addr'
 
 # xclip
 # copy command output directly to clipboard
-alias cb='xsel --clipboard --input'
+alias alias-cb='xsel --clipboard --input'
 
 # alacritty
 alias dup='alacritty --working-directory=$(pwd) &'
@@ -30,13 +22,5 @@ alias dc='docker rm -f $(docker ps -aq) || true; docker ps -aq'
 
 # aws cloudwatch log explorer
 # usage: `output=$(aws-log-explorer --profile localstack --endpoint http://localhost:4566) && echo "$output" | nvim -`
-alias aws-log-explorer='~/.terminal/scripts/aws-cloudwatch-explorer.sh'
-alias aws-delete-log-groups='~/.terminal/scripts/aws-cloudwatch-delete-log-groups.sh'
-
-alias citrix='/opt/Citrix/ICAClient/selfservice'
-
-alias citrix-firejail='firejail --whitelist=/var/lib/snapd/desktop/dconf/profile --whitelist='${HOME}/.ICAClient' --noprofile /opt/Citrix/ICAClient/selfservice'
-
-alias keep-alive-browser='~/Applications/Chromium-stable-136.0.7103.59-x86_64.AppImage --remote-debugging-port=9222 --profile-dir="/home/base/.config/chromium/Default"'
-
-alias keep-alive-runner='node ~/Documents/GitHub/teams-keep-alive/src/index.js 4 --browser-debug-port=9222 --randomize=60'
+alias alias-aws-log-explorer='~/.terminal/scripts/aws-cloudwatch-explorer.sh'
+alias alias-aws-delete-log-groups='~/.terminal/scripts/aws-cloudwatch-delete-log-groups.sh'
