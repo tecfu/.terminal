0e591d1489a2858bccf59709f3e6199cc35e382c
tecfu <help@tecfu.com>
2025-09-21 15:17:44 -0700

    feat: Add commit env vars

--- profile-env-public.sh
+++ profile-env-public.sh
@@ -13,3 +13,16 @@ export HISTCONTROL=ignoreboth
 
 export EDITOR='nvim'
 export VISUAL='nvim'
+
+# Add user's private bin to PATH
+export PATH="$HOME/.local/bin:$PATH"
+
+# For Golang
+export PATH="$HOME/go/bin:$PATH"
+
+# For Rust package manager, Cargo
+. "$HOME/.cargo/env"
+
+# For Volta
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
